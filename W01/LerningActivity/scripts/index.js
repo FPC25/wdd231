11031d3aea974bcb47164db5de511c3b8d4d13ee
FPC25 <felipepcorral@gmail.com>
2025-09-04 09:31:57 -0300

    Add some commentaries to js file and add lastModified functionality

--- W01/LerningActivity/scripts/index.js
+++ W01/LerningActivity/scripts/index.js
@@ -1,6 +1,19 @@
+// creating DOM elements 
+const currentYear = document.querySelector("#currentYear");
+const lastModified = document.querySelector("#lastModified");
 const navButton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar')
 
+// creating a new date object
+const today = new Date();
+
+// getting the current year
+currentYear.textContent = today.getFullYear();
+
+// getting the last modified date
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+// adding show up when clicked behavior to hamburger button 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
   navBar.classList.toggle('show');
