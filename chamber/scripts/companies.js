654031354cdce2b75c1d360e58385365160bf58f
FPC25 <felipepcorral@gmail.com>
2025-09-13 00:57:55 -0300

    Refactor company data fetching and display logic; enhance member status representation

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -1,49 +1,95 @@
 const url = 'https://fpc25.github.io/wdd231/chamber/data/members.json';
 
-const cards = document.querySelector('#cards');
+const gridButton = document.querySelector('#grid-btn');
+const listButton = document.querySelector('#list-btn');
+const companies = document.querySelector('#companies');
 
-async function getProphetData() {
+async function getCompanyData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data);
-    displayProphets(data.members);
+    displayCompanies(data.members);
 }
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
+const displayCompanies = (Companies) => {
+    Companies.forEach((company) => {
         let card = document.createElement('section');
         let namePlace = document.createElement('div');
-        let fullName = document.createElement('h2');
-        let birthdate = document.createElement('p');
-        let birthplace = document.createElement('p');
-        let portrait = document.createElement('img');
+        let name = document.createElement('h2');
+        let memberStatus = document.createElement('img');
+        let address = document.createElement('p');
+
+        let photoPlace = document.createElement('div');
+        let logo = document.createElement('img');
         
-        let name = `${prophet.name} ${prophet.lastname}`;
-        fullName.textContent = name;
+        let infoPlace = document.createElement('div');
+        let phone = document.createElement('p');
+        let contact = document.createElement('p');
+        let url = document.createElement('a');
+        
 
-        namePlace.appendChild(fullName);
+        // populating company title
+        name.textContent = `${company.name}`;
+        address.textContent = `${company.address}`;
 
-        birthdate.innerHTML = `<strong>Date of Birth:</strong> ${prophet.birthdate}`;
+        namePlace.appendChild(name);
+        namePlace.appendChild(address)
 
-        birthdate.id = 'date';
-        birthplace.innerHTML = `<strong>Place of Birth:</strong> ${prophet.birthplace}`;
-        birthplace.id = 'place';
+        let status = company.membershipLevel
+        if (status === 3) {
+            memberStatus.classList.add('gold');
+        } else if (status === 2) {
+            memberStatus.classList.add('silver');
+        } else {
+            memberStatus.classList.add('member');
+        }
 
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${name}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        memberStatus.setAttribute('src', `images/memberCard`);
+        memberStatus.setAttribute('alt', `Member status card`);
+        memberStatus.setAttribute('loading', 'lazy');
+        memberStatus.setAttribute('width', '16');
+        memberStatus.setAttribute('height', '16');
 
+        namePlace.appendChild(memberStatus);
+
+        // populating company logo
+        logo.setAttribute(company.image);
+        logo.setAttribute('alt', `${company.name} trademark`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '1024');
+        logo.setAttribute('height', '1024');
+
+        photoPlace.appendChild(logo);
+
+        // populating company info
+        phone.innerHTML = `<strong>Phone:</strong> ${company.phone}`;
+        contact.innerHTML = `<strong>Contact:</strong> ${company.contact}`;
+
+        url.setAttribute('href', company.website);
+        url.setAttribute('target', '_blank');
+        url.textContent = "Company webpage";
+        
+        infoPlace.appendChild(phone);
+        infoPlace.appendChild(contact);
+        infoPlace.appendChild(url);
+
+        // populating the card
         card.classList.add('card');
 
         card.appendChild(namePlace);
-        card.appendChild(birthdate);  
-        card.appendChild(birthplace);
-        card.appendChild(portrait);
+        card.appendChild(photoPlace);  
+        card.appendChild(infoPlace);
 
+        // populating cards
         cards.appendChild(card);
     });
 }
 
-getProphetData();
+function toggleView(view) {
+    companies.classList.toggle('list', view === 'list');
+    companies.classList.toggle('grid', view === 'grid');
+}
+
+getCompanyData();
+
+listButton.addEventListener("click", () => toggleView('list'));
+gridButton.addEventListener("click", () => toggleView('grid'));
