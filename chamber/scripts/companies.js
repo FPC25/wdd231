1d055c280e2602dc98832362608f75a9df50f975
FPC25 <felipepcorral@gmail.com>
2025-09-12 21:47:11 -0300

    Add functionality to fetch and display member data from JSON

---
+++ chamber/scripts/companies.js
@@ -0,0 +1,49 @@
+const url = 'https://fpc25.github.io/wdd231/chamber/data/members.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data);
+    displayProphets(data.members);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let namePlace = document.createElement('div');
+        let fullName = document.createElement('h2');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let portrait = document.createElement('img');
+        
+        let name = `${prophet.name} ${prophet.lastname}`;
+        fullName.textContent = name;
+
+        namePlace.appendChild(fullName);
+
+        birthdate.innerHTML = `<strong>Date of Birth:</strong> ${prophet.birthdate}`;
+
+        birthdate.id = 'date';
+        birthplace.innerHTML = `<strong>Place of Birth:</strong> ${prophet.birthplace}`;
+        birthplace.id = 'place';
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.classList.add('card');
+
+        card.appendChild(namePlace);
+        card.appendChild(birthdate);  
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
