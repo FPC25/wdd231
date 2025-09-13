39f4bf63ccb4fc576ecb0c81d30a48fada475b57
FPC25 <felipepcorral@gmail.com>
2025-09-13 16:42:13 -0300

    Refactor displayListCompanies: consolidate contact information and enhance member status display

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -109,14 +109,12 @@ const displayListCompanies = (Companies) => {
         let name = document.createElement('h2');
         let address = document.createElement('p');
         let memberStatus = document.createElement('p');
-        let phone = document.createElement('p');
         let contact = document.createElement('p');
         let url = document.createElement('a');
 
         name.textContent = `${company.name}`;
         address.textContent = `${company.address}`;
-        phone.textContent = `${company.phone}`;
-        contact.textContent = `${company.contact}`;
+        contact.textContent = `${company.contact} | ${company.phone}`;
 
         url.setAttribute('href', company.website);
         url.setAttribute('target', '_blank');
@@ -124,17 +122,16 @@ const displayListCompanies = (Companies) => {
 
         let status = company.membershipLevel;
         if (status === 3) {
-            memberStatus.textContent = `Gold`;
+            memberStatus.textContent = `Membership Level: Gold`;
         } else if (status === 2) {
-            memberStatus.textContent = `Silver`;
+            memberStatus.textContent = `Membership Level: Silver`;
         } else {
-            memberStatus.textContent = `Member`;
+            memberStatus.textContent = `Membership Level: Member`;
         }
 
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(memberStatus);
-        card.appendChild(phone);
         card.appendChild(contact);
         card.appendChild(url);
 
