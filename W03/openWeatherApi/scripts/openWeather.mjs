6121238a54558a2e91491db22282cfd1041491ce
FPC25 <felipepcorral@gmail.com>
2025-09-17 21:51:31 -0300

    Refactor weather.js to import config from local openWeather.mjs and enhance API fetch functionality

---
+++ W03/openWeatherApi/scripts/openWeather.mjs
@@ -0,0 +1,3 @@
+export const config = {
+  "appid": 'a0aa2b9410fc67d3a2990a1705c7059c'
+}
--- W03/openWeatherApi/scripts/weather.js
+++ W03/openWeatherApi/scripts/weather.js
@@ -1,4 +1,4 @@
-import { config } from '../../../scripts/OpenWeather.mjs';
+import { config } from './openWeather.mjs';
 
 // DOM elements
 const temp = document.querySelector('#current-temp');
@@ -13,5 +13,25 @@ const lon = 6.64;
 const apiKey = config.appid
 
 // OpenWeather api
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=en`;
+console.log(url)
 
+// Fetching OpenWeather info
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Weather data:', data);
+            return data;
+        } else {
+            const errorText = await response.text();
+            throw new Error(errorText);
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return null;
+    }
+}
+
+apiFetch();
