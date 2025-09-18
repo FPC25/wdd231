ff0d0673ce928283903b00146cadfacebeec3d25
FPC25 <felipepcorral@gmail.com>
2025-09-17 22:48:59 -0300

    Renaming display module to displayWeather so it can be more specific and avoid conflict with other modules

--- W03/openWeatherApi/scripts/displayWeather.mjs
+++ W03/openWeatherApi/scripts/displayWeather.mjs
@@ -5,6 +5,9 @@ const caption = document.querySelector('figcaption');
 
 export function displayWeather(data) {
     temp.innerHTML = `${data.main.temp}&deg;C`;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = `${data.weather[0].description}`
+    icon.setAttribute('SRC', iconsrc);
+    icon.setAttribute('ALT', desc);
+    caption.textContent = desc;
 }
