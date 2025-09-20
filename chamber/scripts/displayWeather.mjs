1e04220b0b6cbf6b2c1f24c0cbed4672f7c33a6e
FPC25 <felipepcorral@gmail.com>
2025-09-20 12:42:25 -0300

    Refactor displayWeather function to improve temperature formatting and ensure min/max temperatures are displayed correctly

--- chamber/scripts/displayWeather.mjs
+++ chamber/scripts/displayWeather.mjs
@@ -1,10 +1,12 @@
 // DOM elements
 const weatherIcon = document.querySelector('#weatherIcon');
 const weatherCaption = document.querySelector('#weatherCaption');
+const minMax = document.querySelector('#minMax');
 const infoPlace = document.querySelector('#infoPlace');
 
 export function displayWeather(data) {
-    
+    let main = data.main
+
     //getting Icon and Caption
     const iconSRC = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     const desc = `${data.weather[0].description}`
@@ -13,6 +15,7 @@ export function displayWeather(data) {
     weatherIcon.setAttribute('SRC', iconSRC);
     weatherIcon.setAttribute('ALT', desc);
     weatherCaption.textContent = desc
+    minMax.innerHTML = `${main.temp_min.toFixed(0)}&deg;C / ${main.temp_max.toFixed(0)}&deg;C`;
 
     //Creating elements to infoPlace
     let currentTemp = document.createElement('p');
@@ -20,10 +23,9 @@ export function displayWeather(data) {
     let feelsLikeTemp = document.createElement('p');
 
     //populating infoPlace
-    let main = data.main
-    currentTemp.innerHTML = `<strong>Temperature</strong>: ${main.temp}&deg;C`;
+    currentTemp.innerHTML = `<strong>Temperature</strong>: ${main.temp.toFixed(0)}&deg;C`;
     humidity.innerHTML = `<strong>Humidity</strong>:${main.humidity}%`;
-    feelsLikeTemp.innerHTML = `<strong>Feels Like</strong>:${main.humidity}&deg;C`;
+    feelsLikeTemp.innerHTML = `<strong>Feels Like</strong>:${main.feels_like.toFixed(0)}&deg;C`;
 
     infoPlace.appendChild(currentTemp);
     infoPlace.appendChild(humidity);
