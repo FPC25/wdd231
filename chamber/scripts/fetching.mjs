c0bafd1497d5365d2c2ce767056b5d93554585db
FPC25 <felipepcorral@gmail.com>
2025-09-19 23:32:46 -0300

    Refactor apiFetch function to accept URL parameter and log data instead of displaying it

--- chamber/scripts/fetching.mjs
+++ chamber/scripts/fetching.mjs
@@ -1,10 +1,11 @@
 // Fetching OpenWeather info
-export async function apiFetch(display) {
+export async function apiFetch(url, display) {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            display(data);
+            console.log(data);
+            // display(data);
         } else {
             const errorText = await response.text();
             throw new Error(errorText);
