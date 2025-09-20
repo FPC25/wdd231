ec65fc3d81be1c6d008b8f1fe1a2e999d5f7220d
FPC25 <felipepcorral@gmail.com>
2025-09-20 12:55:54 -0300

    Refactor displayForecast function to format and display min/max temperatures for the upcoming days

--- chamber/scripts/displayForecast.mjs
+++ chamber/scripts/displayForecast.mjs
@@ -1,15 +1,24 @@
+import { getMinMax } from "./minMaxForecast.mjs";
+
 // DOM elements
-const temp = document.querySelector('#current-temp');
-const icon = document.querySelector("#weather-icon");
-const caption = document.querySelector('figcaption');
-const town = document.querySelector("#town");
+const forecastInfo = document.querySelector('#forecast');
+
+function minMaxFormat(forecast, index) {
+    return `<strong>${forecast[index].weekday}</strong>: ${forecast[index].min_temp.toFixed(0)}&deg;C / ${forecast[index].max_temp.toFixed(0)}&deg;C`;
+}
 
 export function displayForecast(data) {
-    town.textContent = `${data.name}`;
-    temp.innerHTML = `${data.main.temp}&deg;C`;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    let desc = `${data.weather[0].description}`
-    icon.setAttribute('SRC', iconsrc);
-    icon.setAttribute('ALT', desc);
-    caption.textContent = desc;
+    const forecast = getMinMax(data);
+    
+    let tomorrow = document.createElement('p');
+    let after = document.createElement('p');
+    let afterAfter = document.createElement('p');
+
+    tomorrow.innerHTML = minMaxFormat(forecast, 0);
+    after.innerHTML = minMaxFormat(forecast, 1);
+    afterAfter.innerHTML = minMaxFormat(forecast, 2);
+
+    forecastInfo.appendChild(tomorrow);
+    forecastInfo.appendChild(after);
+    forecastInfo.appendChild(afterAfter);
 }
