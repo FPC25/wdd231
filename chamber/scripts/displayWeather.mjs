7d4b9a050dcb9e92be0b3d8b5782cfc27ecd342f
FPC25 <felipepcorral@gmail.com>
2025-09-20 10:23:07 -0300

    Refactor displayWeather function to use consistent DOM elements and dynamically populate the current weather section with info from openWeather API

--- chamber/scripts/displayWeather.mjs
+++ chamber/scripts/displayWeather.mjs
@@ -1,15 +1,31 @@
 // DOM elements
-const temp = document.querySelector('#current-temp');
-const icon = document.querySelector("#weather-icon");
-const caption = document.querySelector('figcaption');
-const town = document.querySelector("#town");
+const weatherIcon = document.querySelector('#weatherIcon');
+const weatherCaption = document.querySelector('#weatherCaption');
+const infoPlace = document.querySelector('#infoPlace');
 
 export function displayWeather(data) {
-    town.textContent = `${data.name}`;
-    temp.innerHTML = `${data.main.temp}&deg;C`;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    let desc = `${data.weather[0].description}`
-    icon.setAttribute('SRC', iconsrc);
-    icon.setAttribute('ALT', desc);
-    caption.textContent = desc;
+    
+    //getting Icon and Caption
+    const iconSRC = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const desc = `${data.weather[0].description}`
+    
+    //Populating Icon elements
+    weatherIcon.setAttribute('SRC', iconSRC);
+    weatherIcon.setAttribute('ALT', desc);
+    weatherCaption.textContent = desc
+
+    //Creating elements to infoPlace
+    let currentTemp = document.createElement('p');
+    let humidity = document.createElement('p');
+    let feelsLikeTemp = document.createElement('p');
+
+    //populating infoPlace
+    let main = data.main
+    currentTemp.innerHTML = `<strong>Temperature</strong>: ${main.temp}&deg;C`;
+    humidity.innerHTML = `<strong>Humidity</strong>:${main.humidity}%`;
+    feelsLikeTemp.innerHTML = `<strong>Feels Like</strong>:${main.humidity}&deg;C`;
+
+    infoPlace.appendChild(currentTemp);
+    infoPlace.appendChild(humidity);
+    infoPlace.appendChild(feelsLikeTemp);
 }
