d4afbd62795182a38293d7bd738418c49826d4fe
FPC25 <felipepcorral@gmail.com>
2025-09-19 23:32:40 -0300

    Fix apiFetch call to include URL for OpenWeather API data retrieval

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -12,4 +12,4 @@ const apiKey = config.appid;
 // OpenWeather api
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=en`;
 
-apiFetch(displayWeather);
+apiFetch(url, displayWeather);
