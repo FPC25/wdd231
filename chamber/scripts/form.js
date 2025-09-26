80b5d2718d69482f99e08cc0dbdbbf14a1bef0e1
FPC25 <felipepcorral@gmail.com>
2025-09-26 00:56:55 -0300

    Make recipient name bold in subscription confirmation message

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -2,8 +2,8 @@ const results = document.querySelector('#results');
 const myInfo = new URLSearchParams(window.location.search)
 
 results.innerHTML = `
-    <p>Dear ${capitalizeFirstLetter(myInfo.get('first'))} ${capitalizeFirstLetter(myInfo.get('last'))},</p>
-    <p>Thank you for subscribing your company, <strong>${myInfo.get('organization-name')}</strong>, as a <strong>${myInfo.get('level').toLowerCase()}</strong> member of the São Paulo Chamber of Commerce. We are thrilled to have you join us!</p>
+    <p>Dear <strong>${capitalizeFirstLetter(myInfo.get('first'))} ${capitalizeFirstLetter(myInfo.get('last'))}</strong>,</p>
+    <p>Thank you for subscribing your company, <strong>${myInfo.get('organization-name')}</strong>, as a <strong>${myInfo.get('level').toLowerCase()}</strong> level member of the São Paulo Chamber of Commerce. We are thrilled to have you join us!</p>
     <p>Your membership was successfully registered on <strong>${myInfo.get('timestamp')}</strong>.</p>
     <p>Here is the contact information you provided:</p>
     <ul>
