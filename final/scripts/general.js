06c6f4a490944bec801732056c90c5f66110d291
FPC25 <felipepcorral@gmail.com>
2025-10-06 10:58:24 -0300

    Improve code readability by adding comments for module imports and DOMContentLoaded event

--- final/scripts/general.js
+++ final/scripts/general.js
@@ -1,7 +1,14 @@
+// Import the DateManager module from the specified file
 import { DateManager } from "./modules/DateManager.mjs";
+
+// Import the MenuManager module from the specified file
 import { MenuManager } from "./modules/MenuManager.mjs";
 
+// Add an event listener to the document that triggers when the DOM content is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
+    // Create a new instance of MenuManager to manage menu-related functionality
     new MenuManager();
+    
+    // Create a new instance of DateManager to manage date-related functionality
     new DateManager();
 });
